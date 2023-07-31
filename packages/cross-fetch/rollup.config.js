8370222c96bd7604482dce30e0c786baed213b01
hubert <hubert@lejian.com>
2023-07-31 09:25:19 +0800

    chore: remove cross-fetch

--- packages/cross-fetch/rollup.config.js
+++
@@ -1,3 +0,0 @@
-import baseConfig from '../../scripts/rollup.base';
-
-export default baseConfig('index', 'AceFetch.CrossFetch');
--- packages/cross-fetch/src/types.ts
+++
@@ -1,7 +0,0 @@
-/// <reference lib="dom" />
-
-declare module '@ace-fetch/core' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface RequestConfig extends Request {}
-}
-
