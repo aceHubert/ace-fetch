9f692f6c2db08fb0f8ec532a08f817ac481e4e8c
hubert <hubert@lejian.com>
2024-11-22 10:46:06 +0800

    feat: add loading status from function param

--- packages/core/playground/apis/index.ts
+++ packages/core/playground/apis/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { registApi, typedUrl } from '@ace-fetch/core';
+import { registApi, typedUrl, createLoadingPlugin } from '@ace-fetch/core';
 
 const axiosInstance = axios.create({
   timeout: 10000,
@@ -12,13 +12,17 @@ export type User = {
   city: string;
 };
 
-export const userApi = registApi(
-  axiosInstance,
-  {
-    getUsers: typedUrl<User[]>`get /users`,
-    getUser: typedUrl<User, { id: string | number }>`/user/${(params: { id: string }) => params.id}`,
-    addUser: typedUrl<User, any, Partial<Omit<User, 'id'>>>`post /user`,
-    delUser: typedUrl<never, { id: string | number }>`delete /user/${'id'}`,
-  },
-  'http://localhost:7009',
-);
+const loadingPlugin = createLoadingPlugin({});
+
+export const userApi = loadingPlugin({
+  registApis: registApi(
+    axiosInstance,
+    {
+      getUsers: typedUrl<User[]>`get /users`,
+      getUser: typedUrl<User, { id: string | number }>`/user/${(params: { id: string }) => params.id}`,
+      addUser: typedUrl<User, any, Partial<Omit<User, 'id'>>>`post /user`,
+      delUser: typedUrl<never, { id: string | number }>`delete /user/${'id'}`,
+    },
+    'http://localhost:7009',
+  ),
+});
