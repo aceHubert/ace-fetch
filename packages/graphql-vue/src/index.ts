061232f4879c1dc062e27eaac28e42c9f2e1614f
hubert <hubert@lejian.com>
2024-12-11 14:49:33 +0800

    fix:  version compare without patch

--- packages/graphql-vue/src/index.ts
+++ packages/graphql-vue/src/index.ts
@@ -2,7 +2,12 @@ import { warn } from '@ace-util/core';
 import { version as coreVersion } from '@ace-fetch/graphql';
 import { version } from './version';
 
-warn(coreVersion === version, `"@ace-fetch/graphql" version mismatch, expected ${version} but ${coreVersion}!`);
+let coreVersionArr = coreVersion.split('.'),
+  versionArr = version.split('.').slice(0, 1); // version without patch
+warn(
+  versionArr.every((v, i) => v === coreVersionArr[i]),
+  `"@ace-fetch/graphql" version mismatch, expected ${versionArr.join('.')}.x but ${coreVersion}!`,
+);
 
 export { version };
 export * from './core';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -2,7 +2,12 @@ import { warn } from '@ace-util/core';
 import { version as coreVersion } from '@ace-fetch/core';
 import { version } from './version';
 
-warn(coreVersion === version, `"@ace-fetch/core" version mismatch, expected ${version} but ${coreVersion}!`);
+let coreVersionArr = coreVersion.split('.'),
+  versionArr = version.split('.').slice(0, 1); // version without patch
+warn(
+  versionArr.every((v, i) => v === coreVersionArr[i]),
+  `"@ace-fetch/core" version mismatch, expected ${versionArr.join('.')}.x but ${coreVersion}!`,
+);
 
 export { version };
 export * from './core';
